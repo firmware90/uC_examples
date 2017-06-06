65b90f3d086dd5e795610bc30a112b49a8d27086
sckim <dr.soochankim@gmail.com>
2017-06-06 21:31:14 +0900

    Update

--- 71_Timer1Overflow/Timer1Overflow.c
+++ 71_Timer1Overflow/Timer1Overflow.c
@@ -1,11 +1,11 @@
-/***********************************************************
-* Purpose: 1초마다 overflow interrupt
+/*************************************
+* Purpose: 1초마다 overflow interrupt를 이용하여 
+* LED Shift
 *
 * TIMSK
 * TCCR1B
 * TCNT1
 *************************************/
-//1초 마다 led 시프트(배열이용)
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -16,7 +16,7 @@ unsigned char LED_ARRAY[9] = { 0xff, 0xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf,
 volatile unsigned int index = 0;
 
 ISR (TIMER1_OVF_vect)	{
-// SIGNAL은 AVR studio v.7에서는 컴파일 안됨
+// SIGNAL은 AVR studio v.7에서는 컴파일 안 됨
 // SIGNAL(SIG_OVERFLOW1) {
 	PORTA = LED_ARRAY[index];
 	index++;
@@ -30,7 +30,7 @@ int main(void) {
 
 	// TOIE1: Timer Overflow Interrupt Enable (Timer 1);
 	// If this bit is set and if global interrupts are enabled, the micro will jump to // the Timer Overflow 1 interrupt vector upon Timer 1 Overflow.
-	TIMSK |= 1 << TOIE1;
+	TIMSK |= (1 << TOIE1);
 	//TIMSK = 0x04;
 	
 	/* Clock Seclects bits
@@ -44,7 +44,8 @@ int main(void) {
 	110: increment timer 1 on T1 Pin falling edge
 	111: increment timer 1 on T1 Pin rising edge
 	*/
-	TCCR1B |= (1<<CS12) | (1<<CS10);  // /1024
+	TCCR1B |= (1<<CS12);
+	TCCR1B |= (1<<CS10);  // /1024
 
 	TCNT1 = 0xFFFF - cOneSecond + 1;
 
